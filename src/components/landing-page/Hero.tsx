610f0e21be98e8499f9feb5a0e3a8aab46fcf6c7
Nichtmetall <hofmannanton@outlook.de>
2025-02-14 00:32:11 +0100

    Added youtube video to hero

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -1,16 +1,26 @@
 "use client";
 
 import FadeMoveUp from "@/lib/FadeMoveUp";
-import { ArrowDown, ArrowDownRight, ArrowUpRight, HomeIcon } from "lucide-react";
+import { ArrowDown, ArrowUpRight, HomeIcon } from "lucide-react";
 import { Fade } from "react-awesome-reveal";
 import BookingDialog from "../dialogs/BookingDialog";
 import Chip from "../other/Chip";
 import TypographyH1 from "../typography/TypographyH1";
 import TypographyLead from "../typography/TypographyLead";
-import { Button } from "../ui/button";
+import YouTube from 'react-youtube';
 import Navlink from "../Navlink";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
+  const videoOptions = {
+    playerVars: {
+      controls: 0,
+      rel: 0,
+      showinfo: 0,
+      loop: 1
+    }
+  };
+
   return (
     <div className="flex items-center justify-between pt-20">
       <div className="flex flex-col md:w-1/2 gap-6">
@@ -22,12 +32,12 @@ export default function Hero() {
         </Fade>
         <FadeMoveUp>
           <TypographyH1 className="hypens-manual">
-            Deine Prozesse - digital und automatisiert. 
+            Deine Prozesse - digital und automatisiert.
           </TypographyH1>
         </FadeMoveUp>
         <FadeMoveUp delay={100}>
           <TypographyLead>
-          Mehr Bewertungen, höhere Reichweite, gesteigertes Vertrauen – ohne zusätzlichen Aufwand für Dich.</TypographyLead>
+            Mehr Bewertungen, höhere Reichweite, gesteigertes Vertrauen – ohne zusätzlichen Aufwand für Dich.</TypographyLead>
         </FadeMoveUp>
         <div className="flex gap-4 md:flex-row flex-col">
           <FadeMoveUp delay={200}>
@@ -46,8 +56,9 @@ export default function Hero() {
       </div>
       <div className="hidden md:flex items-center justify-center">
         <Fade triggerOnce duration={2000}>
-          <img src="/fox_white.svg" className="hidden dark:flex w-72" />
-          <img src="/fox_dark.svg" className="dark:hidden w-72" />
+          <div className="py-12 pl-24">
+            <YouTube videoId="ZxlZgsOQUEc" opts={videoOptions} />
+          </div>
         </Fade>
       </div>
     </div>
--- src/components/landing-page/Portfolio.tsx
+++ src/components/landing-page/Portfolio.tsx
@@ -17,30 +17,39 @@ export default function Portfolio() {
                     <div className="bg-gray-600 rounded-lg w-full h-52" />
                     <div className="text-center flex flex-col gap-2">
                         <TypographyH2>Testprojekt 1</TypographyH2>
-                        <TypographyLead>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</TypographyLead>
+                        <TypographyLead>
+                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+                            Ipsum Lorem Ipsum Lorem Ipsum
+                        </TypographyLead>
                     </div>
                     <div className="flex justify-center">
-                        <Button size={'lg'}>Mehr lesen</Button>
+                        <Button size={"lg"}>Mehr lesen</Button>
                     </div>
                 </div>
                 <div className="flex flex-col gap-8">
                     <div className="bg-gray-600 rounded-lg w-full h-52" />
                     <div className="text-center flex flex-col gap-2">
                         <TypographyH2>Testprojekt 2</TypographyH2>
-                        <TypographyLead>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</TypographyLead>
+                        <TypographyLead>
+                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+                            Ipsum Lorem Ipsum Lorem Ipsum
+                        </TypographyLead>
                     </div>
                     <div className="flex justify-center">
-                        <Button size={'lg'}>Mehr lesen</Button>
+                        <Button size={"lg"}>Mehr lesen</Button>
                     </div>
                 </div>
                 <div className="flex flex-col gap-8">
                     <div className="bg-gray-600 rounded-lg w-full h-52" />
                     <div className="text-center flex flex-col gap-2">
                         <TypographyH2>Testprojekt 3</TypographyH2>
-                        <TypographyLead>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum</TypographyLead>
+                        <TypographyLead>
+                            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+                            Ipsum Lorem Ipsum Lorem Ipsum
+                        </TypographyLead>
                     </div>
                     <div className="flex justify-center">
-                        <Button size={'lg'}>Mehr lesen</Button>
+                        <Button size={"lg"}>Mehr lesen</Button>
                     </div>
                 </div>
             </div>
